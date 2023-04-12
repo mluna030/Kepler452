2047c50f772490ef514f452287bcbc6a0e4a66ba
Michael A Luna <michael.luna37@gmail.com>
2023-04-11 22:15:58 -0700

    Update server_Quantum.py

--- server_Quantum.py
+++ server_Quantum.py
@@ -18,7 +18,7 @@ keypair = random_wmss(4)
 
 # Set up a socket server
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('localhost', 12345))
+server_socket.bind(('192.168.0.100', 5000))
 server_socket.listen(1)
 
 print("Server is waiting for connections...")
